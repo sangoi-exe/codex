afe63e9db87aa972729d19e55ef789654f395060
sangoi <sangoi.exe@gmail.com>
2025-09-22 10:09:02 -0300

    chore(repo): ignore and untrack local .cargo-home cache

--- .cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/schemafy-0.5.2/publish.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-(./version.sh $@ &&
-    (cd schemafy_core && cargo publish) &&
-    sleep 15 &&
-    (cd schemafy_lib && cargo publish) &&
-    sleep 15 &&
-    cargo publish)
--- .cargo-home/registry/src/index.crates.io-1949cf8c6b5b557f/ts-rs-11.0.1/src/tokio.rs
+++
@@ -1,7 +0,0 @@
-use tokio::sync::{Mutex, OnceCell, RwLock};
-
-use super::{impl_wrapper, TypeVisitor, TS};
-
-impl_wrapper!(impl<T: TS> TS for Mutex<T>);
-impl_wrapper!(impl<T: TS> TS for OnceCell<T>);
-impl_wrapper!(impl<T: TS> TS for RwLock<T>);
